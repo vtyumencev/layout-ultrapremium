2a0a49b95a5243127ca7d0c2aa358ad36d13a37d
Вячеслав Тюменцев <Prosto4ok@outlook.com>
2021-10-03 23:16:19 +0500

    First commit

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper-front-banner", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+});
